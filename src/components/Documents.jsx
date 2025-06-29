f4fa42790efaaef6caf492c11f6df57dbbaae7d1
Timo Neske <Timo.Neske@hhu.de>
2025-06-29 10:47:11 +0200

    build project

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -3,10 +3,10 @@ function Documents() {
     return (
         <>
         <div id="documents-container">
-            <a href="/portfolio-website/downloads/Timo%20Neske%20-%20L%C3%9C_3150193.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="./downloads/Timo%20Neske%20-%20L%C3%9C_3150193.pdf" target="_blank" rel="noopener noreferrer">
                 Aktuelle Leistungsübersicht Studium (download 274KB)
             </a>
-            <a href="/portfolio-website/downloads/Timo%20Neske%20-%20Abitur.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="./downloads/Timo%20Neske%20-%20Abitur.pdf" target="_blank" rel="noopener noreferrer">
                 Abiturzeugnis (download 16KB)
             </a>
         </div>
