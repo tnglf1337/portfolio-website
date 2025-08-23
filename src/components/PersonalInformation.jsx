159718343ee7b6d167ac75bd31cd34ece4553857
Timo Neske <neske.timo93@gmail.com>
2025-08-23 19:18:10 +0200

    update personal info

--- src/components/PersonalInformation.jsx
+++ src/components/PersonalInformation.jsx
@@ -2,11 +2,10 @@ function PersonalInformation() {
   return (
     <>
     <div id="personal-container">
-      <p>Ich bin ein 31-jähriger Informatikstudent im sechsten Semester an der Heinrich-Heine-Universität in Düsseldorf.</p>
+      <p>Ich bin ein 31-jähriger Informatikstudent an der Heinrich-Heine-Universität in Düsseldorf und ab Oktober 25 im siebten Semester. Vorraussichtlich schreibe ich im achten Semester meine Bachelorarbeit im Bereich Machine Learning oder Datenbanken und Informationssysteme.</p>
       <p>Bevor ich mein Studium begann, habe ich nach einer Ausbildung zum Altenpfleger mein Abitur nachgeholt.</p>
-      <p>Im Bereich der Informatik interessiere ich mich für verschiedene Themen wie professionelle Softwareentwicklung (insbesondere Softwarearchitektur), theoretische Algorithmen und Bioinformatik.</p>
+      <p>Im Bereich der Informatik interessiere ich mich für verschiedene Themen wie professionelle Softwareentwicklung (insbesondere Softwarearchitektur), theoretische Algorithmen und Machine Learning.</p>
       <p>Darüber hinaus bin ich vielseitig interessiert und lerne gerne Neues. In meiner Freizeit spiele ich gern Badminton und habe ein Faible für Katzen, auch wenn meine Nase manchmal leicht allergisch reagiert...</p>
-      <p>Mein MBTI ist ISTY &#129299;.</p>
     </div>
     </>
   );
