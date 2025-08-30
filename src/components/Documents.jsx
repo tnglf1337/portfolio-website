460f716ea61b0b80d537db61bea03a89301469c3
Timo Neske <neske.timo93@gmail.com>
2025-08-30 17:01:16 +0200

    remove download link

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -6,9 +6,6 @@ function Documents() {
             <a href="Timo%20Neske%20-%20LU_3150193.pdf" target="_blank" rel="noopener noreferrer">
                 Aktuelle Leistungsübersicht Studium (download 274KB)
             </a>
-            <a href="Timo%20Neske%20-%20Abitur.pdf" target="_blank" rel="noopener noreferrer">
-                Abiturzeugnis (download 16KB)
-            </a>
         </div>
         </>
     );
